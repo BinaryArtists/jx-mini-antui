dd51d0ea3f4ab1b19394aca3f5f2c9cf0ed54383
7 <fengzilijie@qq.com>
2019-07-15 14:23:35 +0800

    【DEV】增加 骨架屏案例

---
+++ app/page/custom/skeleton-screen/skeleton-screen.js
@@ -0,0 +1,32 @@
+/**
+ * Skeleton Screen 骨架屏
+ * 
+ * H5 方案
+ * 
+ * 1. 完全靠手写HTML和CSS方式给每个页面定制一套骨架屏
+ * 2. 利用预渲染的方式生成静态骨架屏
+ * 2.1 饿了么：puppeteer，https://github.com/ElemeFE/page-skeleton-webpack-plugin
+ */
+Page({
+  data: {
+		motto: 'Hello World',
+		userInfo: {
+			avatarUrl: 'https://sfault-image.b0.upaiyun.com/117/579/1175792133-5b63fce811636_articlex',
+			nickName: 'jayzou'
+		},
+		lists: [
+			'aslkdnoakjbsnfkajbfk',
+			'qwrwfhbfdvndgndghndeghsdfh',
+			'qweqwtefhfhgmjfgjdfghaefdhsdfgdfh',
+		],
+		showSkeleton: true
+	},
+	onLoad: function () {
+		const that = this;
+		setTimeout(() => {
+			that.setData({
+				// showSkeleton: false
+			})
+		}, 4000)
+	}
+});
